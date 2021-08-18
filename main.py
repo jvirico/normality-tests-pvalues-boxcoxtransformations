1bb8fc970062e07ebae430d91e2fc8a96c4575c2
jvirico <jvirico@gmail.com>
2021-08-18 20:22:14 +0200

    qq plot and histogram of old males weights

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from matplotlib import style
 import statsmodels.api as sm
 from scipy import stats
 
-plt.style.use('ggplot')
+#plt.style.use('ggplot')
 
-import warnings
+#import warnings
 #warnings.filterwarnings('ignore')
 
 # Data
@@ -27,5 +27,39 @@ weight = filtered_data['weight']
 weight.to_csv('./data/filtered_data_weight.csv')
 '''
 
-data = pd.read_csv('./data/filtered_data_weight.csv')
-print(data.head(10))
+raw_data = pd.read_csv('./data/raw_data.csv')
+#print(raw_data.head(10))
+filtered_data = raw_data.copy()
+filtered_data = filtered_data[(filtered_data.age > 15) & filtered_data.male == 1]
+#print(filtered_data.head(5))
+#all_data = raw_data['weight']
+data = filtered_data['weight']
+print(data)
+
+### Graphical representation of data
+#
+# we fit mean and variance to the data
+mu, sigma = stats.norm.fit(data)
+print('Mean: %s and Std: %s' % (mu,sigma))
+# theorical values of normal distribution in the observed range
+print(min(data))
+print(max(data))
+x_hat = np.linspace(min(data),max(data), num=100)
+y_hat = stats.norm.pdf(x_hat, mu, sigma)
+
+fig, ax = plt.subplots(figsize=(7,4))
+ax.plot(x_hat, y_hat, linewidth=2, label='normal')
+ax.hist(x=data, density=True, bins=40, color="#3182bd", alpha=0.5)
+ax.plot(data, np.full_like(data, -0.01), '|k', markeredgewidth=1)
+ax.set_title('Weight distribution (men older than 15 years old)')
+ax.set_xlabel('weight')
+ax.set_ylabel('Probaility Distribution')
+ax.legend()
+plt.show()
+
+### Using Q-Q plot
+fig, ax = plt.subplots(figsize=(7,4))
+sm.qqplot(data,fit=True,line='q',alpha=0.4,lw=2,ax=ax)
+ax.set_title('Q-Q plot of weights of men older than 15 years old')
+ax.tick_params(labelsize=7)
+plt.show()
