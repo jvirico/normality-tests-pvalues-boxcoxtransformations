55f86e8ba6b86bfde0e7ab64cdfbf991a20745e8
jvirico <jvirico@gmail.com>
2021-08-18 19:44:57 +0200

    saving data locally

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ from scipy import stats
 plt.style.use('ggplot')
 
 import warnings
-warnings.filterwarnings('ignore')
+#warnings.filterwarnings('ignore')
 
 # Data
+'''
 url = ('https://raw.githubusercontent.com/JoaquinAmatRodrigo/' +
        'Estadistica-machine-learning-python/master/data/Howell1.csv')
-datos = pd.read_csv(url)
-print(datos.info())
-datos.head(4)
+raw_data = pd.read_csv(url)
+raw_data.to_csv('./data/raw_data.csv')
+print(raw_data.info())
+raw_data.head(4)
+
+filtered_data = raw_data[(raw_data.age > 15) & (raw_data.male == 0)]
+weight = filtered_data['weight']
+weight.to_csv('./data/filtered_data_weight.csv')
+'''
+
+data = pd.read_csv('./data/filtered_data_weight.csv')
+print(data.head(10))
